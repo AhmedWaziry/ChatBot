5ed01d808fb888f8a9a51ffc999ecc04432077d4
Ahmed Gamal <ahmed.hassan@bld.ai>
2023-05-03 03:29:25 +0300

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     cssCodeSplit: false,
   },
