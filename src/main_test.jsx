efc677292c7a7b813f2b3eb861e260bf215aa67c
Ahmed Gamal <ahmed.hassan@bld.ai>
2023-05-03 21:14:48 +0300

    build again

--- src/main_test.jsx
+++ src/main_test.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppChat from "./AppChat";
+const element = document.createElement("dev");
+element.setAttribute("id", "usualchat-ahmed");
+document.body.appendChild(element);
 
-ReactDOM.render(<AppChat />, document.body);
+ReactDOM.render(<AppChat />, document.getElementById("usualchat-ahmed"));
