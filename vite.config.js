4476914896034d9e69349f5195128fba54018cfc
Ahmed Gamal <ahmed.hassan@bld.ai>
2023-05-03 19:41:51 +0300

    build again

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       output: {
         format: "es",
         strict: false,
-        entryFileNames: "assets/chatpage_ahmedgamal7.js",
+        entryFileNames: "assets/chatpage_ahmedgamal8.js",
         dir: "dist",
       },
     },
