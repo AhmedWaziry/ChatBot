2afe85dc52ed05181c55825670647bb3d009996f
Ahmed Gamal <ahmed.hassan@bld.ai>
2023-05-03 20:10:50 +0300

    build again

--- src/main_test.jsx
+++ src/main_test.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppChat from "./AppChat";
-import "./indexChat.css";
 
 ReactDOM.render(<AppChat />, document.getElementById("root"));
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       output: {
         format: "es",
         strict: false,
-        entryFileNames: "assets/chatpage_ahmedgamal10.js",
+        entryFileNames: "assets/chatpage_ahmedgamal20.js",
         dir: "dist",
       },
     },
