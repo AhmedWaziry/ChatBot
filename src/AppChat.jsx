c75d57fc3c36eb16a4554edcac5a090a808d9929
Ahmed Gamal <ahmed.hassan@bld.ai>
2023-05-04 14:36:34 +0300

    build again

--- src/AppChat.jsx
+++ src/AppChat.jsx
@@ -4,11 +4,11 @@ import Extension from "./components/Extension";
 import Style from "./AppChat.module.css";
 
 function AppChat() {
-  const [id, setId] = useState("fc1e41c7-be04-4339-962e-6a9d80dec713");
+  const [id, setId] = useState("");
 
   useEffect(() => {
     const scriptTag = document.querySelector(
-      'script[src="https://cdn.jsdelivr.net/gh/AhmedWaziry/ChatBot@main/dist/assets/chat_ahmedgamal4.js"]'
+      'script[src="https://cdn.jsdelivr.net/gh/AhmedWaziry/ChatBot@main/dist/assets/chat_extension_1.js"]'
     );
 
     if (scriptTag) {
--- src/main_test.jsx
+++ src/main_test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppChat from "./AppChat";
 const element = document.createElement("dev");
-element.setAttribute("id", "usualchat-ahmed");
+element.setAttribute("id", "usualchat-extension");
 document.body.appendChild(element);
 
-ReactDOM.render(<AppChat />, document.getElementById("usualchat-ahmed"));
+ReactDOM.render(<AppChat />, document.getElementById("usualchat-extension"));
