27b2c60f7b8b57522677a6b9f4a08cae886b52d7
Ahmed Gamal <ahmed.hassan@bld.ai>
2023-06-14 16:30:35 +0300

    change the file name

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       output: {
         format: "es",
         strict: false,
-        entryFileNames: "assets/chat_extension_02.js",
+        entryFileNames: "assets/chat_extension_01.js",
         dir: "dist",
       },
     },
