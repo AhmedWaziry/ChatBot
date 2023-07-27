e35f4ccf0085ccdf51e9650cb7e8ded7378a24db
Ahmed Gamal <ahmed.hassan@bld.ai>
2023-07-27 13:49:39 +0300

    build

--- src/AppChat.jsx
+++ src/AppChat.jsx
@@ -4,7 +4,7 @@ import Extension from "./components/Extension";
 import Style from "./AppChat.module.css";
 
 function AppChat() {
-  const [id, setId] = useState("fbc8ccd5-181c-4f7d-826c-d5ce44d8859b");
+  const [id, setId] = useState("");
 
   useEffect(() => {
     const scriptTag = document.querySelector(
